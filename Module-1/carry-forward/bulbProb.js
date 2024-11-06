5ae08c140c35f5432d9f1b4808a465067e86b5f9
Suraj Yadav <suraj.yadav3694@gmail.com>
2024-11-07 00:03:49 +0530

    Added new problem related to bulb

---
+++ Module-1/carry-forward/bulbProb.js
@@ -0,0 +1,17 @@
+/*
+A wire connects N light bulbs.
+Each bulb has a switch associated with it; however, due to faulty wiring, a switch also changes the state of all the bulbs to the right of the current bulb.
+Given an initial state of all bulbs, find the minimum number of switches you have to press to turn on all the bulbs.
+You can press the same switch multiple times.
+Note: 0 represents the bulb is off and 1 represents the bulb is on.
+
+input:
+A = [0, 1, 0, 1]
+
+output:
+4
+
+TC: O(N)
+
+SC: O(1)
+*/
