129f1d5103a4ec51a143df7723acecac06d3d990
Suraj Yadav <suraj.yadav3694@gmail.com>
2024-11-07 17:29:40 +0530

    Added new problem file

---
+++ Module-1/carry-forward/pickBothSide.js
@@ -0,0 +1,23 @@
+/*
+You are given an integer array A of size N.
+You have to perform B operations. In one operation, you can remove either the leftmost or the rightmost element of the array A.
+Find and return the maximum possible sum of the B elements that were removed after the B operations.
+
+NOTE: Suppose B = 3, and array A contains 10 elements, then you can:
+
+Remove 3 elements from front and 0 elements from the back, OR
+Remove 2 elements from front and 1 element from the back, OR
+Remove 1 element from front and 2 elements from the back, OR
+Remove 0 elements from front and 3 elements from the back.
+
+input:
+A = [5, -2, 3 , 1, 2]
+B = 3
+
+output:
+8
+
+TC:
+
+SC:
+*/
