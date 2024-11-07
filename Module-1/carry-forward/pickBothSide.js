ed2643906334aad0fe48b6bc20ae91171ff90ed8
Suraj Yadav <suraj.yadav3694@gmail.com>
2024-11-07 18:26:58 +0530

    #4 Added soln for pick both side max sum problem

--- Module-1/carry-forward/pickBothSide.js
+++ Module-1/carry-forward/pickBothSide.js
@@ -4,7 +4,6 @@ You have to perform B operations. In one operation, you can remove either the le
 Find and return the maximum possible sum of the B elements that were removed after the B operations.
 
 NOTE: Suppose B = 3, and array A contains 10 elements, then you can:
-
 Remove 3 elements from front and 0 elements from the back, OR
 Remove 2 elements from front and 1 element from the back, OR
 Remove 1 element from front and 2 elements from the back, OR
