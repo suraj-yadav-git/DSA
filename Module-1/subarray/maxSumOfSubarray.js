6a316f3747bdc9f69f1b639f5ebd3342bfc16bc0
Suraj Yadav <suraj.yadav3694@gmail.com>
2024-11-09 17:24:49 +0530

    Added max subarray problem with limit

---
+++ Module-1/subarray/maxSumOfSubarray.js
@@ -0,0 +1,38 @@
+/*
+You are given an integer array C of size A. Now you need to find a subarray (contiguous elements) so that the sum of contiguous elements is maximum.
+But the sum must not exceed B.
+
+input:
+A = 5
+B = 12
+C = [2, 1, 3, 4, 5]
+
+
+output:
+12
+
+TC: O()
+SC: O()
+*/
+
+const maxSumOfSubarray = (A, B, C) => {
+    let maxSum = 0;
+    for(let i=0; i<A; i++) {
+        let sum = 0;
+        for(let j=i; j<A; j++) {
+            if(sum + C[j] > B) {
+                break;
+            } else {
+                sum += C[j];
+                if(maxSum < sum) maxSum = sum;
+            }
+        }
+    }
+    return maxSum;
+}
+
+A = 5;
+B = 12;
+C = [2, 1, 3, 4, 5];
+
+console.log(maxSumOfSubarray(A,B,C));
