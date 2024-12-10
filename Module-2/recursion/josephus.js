0566b235e26abfa0c662a8fbfaae4e355675e6de
Suraj Yadav <suraj.yadav3694@gmail.com>
2024-12-10 23:41:37 +0530

    #34 added recursion probs

---
+++ Module-2/recursion/josephus.js
@@ -0,0 +1,16 @@
+/*
+Given the total number of person A and a number B which indicates that B-1 persons are skipped and the Bth person is killed in a circle. 
+Find the last person standing in the circle. As an example, for A = 12, and B = 2,
+
+*/
+
+const josephusProb = (A, B) => {
+    if(A === 1) return 1;
+    return (josephusProb(A-1, B) + B - 1) % A + 1;
+}
+
+const A = 5;
+const B = 2;
+
+console.log(josephusProb(A,B));
+
---
+++ Module-2/recursion/power.js
@@ -0,0 +1,30 @@
+/*
+Implement pow(A, B).
+In other words, given A, B Find (A^B).
+
+Input:
+A = 2
+B = 3
+
+Output:
+8
+
+TC:
+
+SC:
+*/
+
+const getPower = (A, B) => {
+    if(A === 0) return 0;
+    if(B === 0) return 1;
+    if(B % 2 === 0) {
+        return getPower(A, B/2) * getPower(A, B/2);
+    } else {
+        return getPower(A, (B-1)/2) * getPower(A, (B-1)/2) * A;
+    }
+}
+
+const A = 2
+const B = 3
+
+console.log(getPower(A,B));
